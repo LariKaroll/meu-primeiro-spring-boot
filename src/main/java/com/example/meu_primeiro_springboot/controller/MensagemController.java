6b786f2471f700b1dadfc1a2eb52e014208bc1d6
LariKaroll <larikarol108@gmail.com>
2026-04-21 19:46:27 -0300

    Spring-Security

--- src/main/java/com/example/meu_primeiro_springboot/controller/MensagemController.java
+++ src/main/java/com/example/meu_primeiro_springboot/controller/MensagemController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.meu_primeiro_springboot.service.MensagemService;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
